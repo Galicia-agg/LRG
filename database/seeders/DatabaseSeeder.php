8f5558d63830dca61f025aae701ff628dd714b1d
Gustavo Adolfo Galicia Gutierrez <ggalicia@mintrabajo.gob.gt>
2026-09-10 00:30:24 -0600

    fix: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,15 +18,19 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RoleSeeder::class);
 
-        $admin = User::factory()->create([
+        $admin = User::create([
             'name' => 'Administrador',
             'email' => 'admin@aceitera.test',
+            'password' => Hash::make('password'), // Campo obligatorio faltante
+            'email_verified_at' => now(),
         ]);
         $admin->assignRole('admin');
 
         $cajero = User::factory()->create([
             'name' => 'Cajero',
             'email' => 'cajero@aceitera.test',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
         ]);
         $cajero->assignRole('cajero');
 
